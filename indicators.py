1a3c31c855d7607b4dd72de8199d783fec01a731
Sundar Dangi <sundardangi42@gmail.com>
2026-07-24 23:25:16 +0545

    Added RSI and ATR indicators

--- indicators.py
+++ indicators.py
@@ -1,10 +1,13 @@
 import ta
 
+
 def calculate_ema(df):
     """
-    Calculate EMA20 and EMA50
+    Calculate technical indicators
+    EMA20, EMA50, RSI, ATR
     """
 
+    # EMA indicators
     df["EMA20"] = ta.trend.ema_indicator(
         df["close"],
         window=20
@@ -14,5 +17,26 @@ def calculate_ema(df):
         df["close"],
         window=50
     )
+
+
+    # RSI indicator
+    df["RSI"] = ta.momentum.rsi(
+        df["close"],
+        window=14
+    )
+
+
+    # ATR volatility indicator
+    df["ATR"] = ta.volatility.average_true_range(
+        high=df["high"],
+        low=df["low"],
+        close=df["close"],
+        window=14
+    )
+
+
+    # Remove incomplete rows
+    df.dropna(inplace=True)
+
 
     return df
